f3dfe059a1692bfd7640fd037cb7f7bf2498060d
Amandeep Singh <65186944+amandeep90s@users.noreply.github.com>
2026-07-23 20:07:50 +0530

    feat: implement blog creation functionality and update blog listing

--- app/blogs/page.tsx
+++ app/blogs/page.tsx
@@ -1,14 +1,23 @@
-import { blogs } from "../services/blogs";
+import { getBlogs } from "../services/blogs";
 
 const Blogs = () => {
+  const blogs = getBlogs();
   return (
-    <div>
+    <div className="space-y-4">
+      <h1 className="text-3xl font-medium">Blogs</h1>
+
       {blogs.map((blog) => (
-        <div key={blog.id} className="border border-gray-300 p-4 mb-4 rounded">
+        <div
+          key={blog.id}
+          className="border border-gray-300 p-4 mb-4 rounded space-y-4"
+        >
           <h2 className="text-xl font-semibold mb-2">{blog.title}</h2>
           <p>Author: {blog.author}</p>
           <p>Likes: {blog.likes}</p>
-          <a href={blog.url} className="text-blue-500 hover:text-white">
+          <a
+            href={blog.url}
+            className="bg-emerald-600 inline-block text-white py-2 px-4 rounded-sm hover:bg-emerald-700 transition-colors"
+          >
             Read more
           </a>
         </div>
