c968a5c511baa0fc1253bfa9034488a5ba20e930
Amandeep Singh <amandeep90s@users.noreply.github.com>
2026-07-25 23:30:37 +0530

    feat: add password hash to users table and create blogs table with foreign key relationship

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,5 @@
-import Link from "next/link";
-
+import Navbar from "./components/Navbar";
+import AuthSessionProvider from "./components/SessionProvider";
 import "./globals.css";
 
 export default function RootLayout({
@@ -10,36 +10,10 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className="flex flex-col min-h-screen p-4">
-        <nav className="mb-4 border-b border-gray-300 pb-2">
-          <Link
-            href="/"
-            className="text-emerald-500 px-4 hover:text-emerald-300"
-          >
-            Home
-          </Link>
-          {" | "}
-          <Link
-            href="/blogs"
-            className="text-emerald-500 px-4 hover:text-emerald-300"
-          >
-            Blogs
-          </Link>
-          {" | "}
-          <Link
-            href="/users"
-            className="text-emerald-500 px-4 hover:text-emerald-300"
-          >
-            Users
-          </Link>
-          {" | "}
-          <Link
-            href="/blogs/new"
-            className="text-emerald-500 px-4 hover:text-emerald-300"
-          >
-            Add New
-          </Link>
-        </nav>
-        {children}
+        <AuthSessionProvider>
+          <Navbar />
+          {children}
+        </AuthSessionProvider>
       </body>
     </html>
   );
