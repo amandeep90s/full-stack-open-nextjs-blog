f2bbe2db7077a513819c8aedac8e551b9b251540
Amandeep Singh <amandeep90s@users.noreply.github.com>
2026-07-26 22:13:35 +0530

    feat: enhance user registration with validation and error handling

--- app/register/page.tsx
+++ app/register/page.tsx
@@ -1,13 +1,26 @@
 "use client";
 
 import { registerUser } from "@/app/actions/users";
+import { useActionState } from "react";
+
+const initialState = {
+  username: "",
+  name: "",
+  password: "",
+  confirmPassword: "",
+};
 
 export default function RegisterPage() {
+  const [state, formAction] = useActionState(registerUser, {
+    error: "",
+    fields: initialState,
+  });
+
   return (
     <div className="grow space-y-4 flex flex-col items-center justify-center ">
       <h1 className="text-3xl font-medium">Register</h1>
       <form
-        action={registerUser}
+        action={formAction}
         className="space-y-4 border border-gray-300 p-6 rounded w-full max-w-md"
       >
         <div>
@@ -16,6 +29,7 @@ export default function RegisterPage() {
             type="text"
             name="username"
             className="border border-gray-300 rounded-sm p-2 w-full"
+            defaultValue={state.fields?.username ?? ""}
             required
           />
         </div>
@@ -25,6 +39,8 @@ export default function RegisterPage() {
             type="text"
             name="name"
             className="border border-gray-300 rounded-sm p-2 w-full"
+            defaultValue={state.fields?.name ?? ""}
+            minLength={4}
             required
           />
         </div>
@@ -34,6 +50,18 @@ export default function RegisterPage() {
             type="password"
             name="password"
             className="border border-gray-300 rounded-sm p-2 w-full"
+            defaultValue={state.fields?.password ?? ""}
+            minLength={4}
+            required
+          />
+        </div>
+        <div>
+          <label className="block mb-2">Confirm Password</label>
+          <input
+            type="password"
+            name="confirmPassword"
+            className="border border-gray-300 rounded-sm p-2 w-full"
+            defaultValue={state.fields?.confirmPassword ?? ""}
             required
           />
         </div>
@@ -43,6 +71,8 @@ export default function RegisterPage() {
         >
           Register
         </button>
+
+        {state.error && <p className="text-red-500 mt-3">{state.error}</p>}
       </form>
     </div>
   );
